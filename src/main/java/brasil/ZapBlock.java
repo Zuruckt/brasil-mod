1b7244a818da0bdb0cc8458267a11bcbe0b3a0ee
Jeferson Oliveira <zuruckyt@gmail.com>
2023-01-12 23:40:39 -0300

    feat: zap block implementation

---
+++ src/main/java/brasil/ZapBlock.java
@@ -0,0 +1,11 @@
+package brasil;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class ZapBlock extends Block
+{
+    public ZapBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/brasil/ZapBlockItem.java
@@ -0,0 +1,11 @@
+package brasil;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ZapBlockItem extends BlockItem {
+    public ZapBlockItem(Block block, Item.Properties properties) {
+        super(block, properties);
+    }
+}
