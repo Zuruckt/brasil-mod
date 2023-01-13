52f0db512e169baacb9362f1b2123a56ed3af215
Jeferson Oliveira <zuruckyt@gmail.com>
2023-01-12 23:41:16 -0300

    feat: brasil mod core

---
+++ src/main/java/brasil/Brasil.java
@@ -0,0 +1,41 @@
+package brasil;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Brasil.MODID)
+public class Brasil
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "brasil";
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    public Brasil()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        RegistryObject<Block> zapBlock = BLOCKS.register("zap_block", () -> new ZapBlock(BlockBehaviour.Properties.of(Material.STONE)));
+        ITEMS.register("zap_block_item", () -> new ZapBlockItem(zapBlock.get(), new Item.Properties()));
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        BLOCKS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ITEMS.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+}
